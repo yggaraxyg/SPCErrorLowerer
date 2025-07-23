7d3d00b2736b08c16730dbbfa913136404d77b91
YggaRaxyg <yggaraxyg@gmail.com>
2025-07-22 21:00:30 -0700

    Graphs and RunData

---
+++ repackager.py
@@ -0,0 +1,16 @@
+import numpy as np
+import sys
+
+if (len(sys.argv)<3):
+    print("newname and Filename(s) please!")
+    exit(-1)
+
+box = []
+for i in range(len(sys.argv)-2):
+    filename = sys.argv[i+2]
+    curfile = np.load(filename)
+    curerr = curfile['error']
+    for i in range(len(curerr)):
+        box.append(curerr[i])
+    
+np.savez(sys.argv[1], error=box)
